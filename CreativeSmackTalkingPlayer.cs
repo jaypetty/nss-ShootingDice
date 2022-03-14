6aa7f5b376c8d6631672f405a35b0814c17771a9
jaypetty <jayshaun2343@gmail.com>
2022-03-14 14:39:04 -0500

    finished CreativeSmack Class

--- CreativeSmackTalkingPlayer.cs
+++ CreativeSmackTalkingPlayer.cs
@@ -1,8 +1,23 @@
+using System;
+using System.Collections.Generic;
+
 namespace ShootingDice
 {
     // A SmackTalkingPlayer who randomly selects a taunt from a list to say to the other player
-    public class CreativeSmackTalkingPlayer
+    public class CreativeSmackTalkingPlayer : Player
     {
+        List<string> taunts = new List<string> 
+        {
+            "Nice try, Don't do it again!",
+            "A graceful taunt is worth a thousand insults",
+            "Are you sure you know what you're doing?"
 
+        };
+
+        public override int Roll()
+        {
+            Console.WriteLine($"{Name} says: {taunts[new Random().Next(0, taunts.Count)]}");
+            return new Random().Next(DiceSize) + 1;
+        }
     }
 }
